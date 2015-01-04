cf8c65e1f7f7821e5babb445610e4b7c3178f59b
zhou <jelychow@gmail.com>
2015-01-04 23:22:54 +0800

    add baidu map sdk

--- HiDinner/src/com/sec/hidinner/MainActivity.java
+++ HiDinner/src/com/sec/hidinner/MainActivity.java
@@ -1,17 +1,34 @@
 package com.sec.hidinner;
 
+import com.sec.hidinner.location.MyLocation;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
 
+	private Button mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButton = (Button) findViewById(R.id.button1);
+        mButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this,MyLocation.class);
+				startActivity(intent);
+				
+			}
+		});
     }
 
 
