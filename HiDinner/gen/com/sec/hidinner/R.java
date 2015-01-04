6e4d0a7e1c5ec595026c47f52cd3c754433be639
zhou <jelychow@gmail.com>
2015-01-04 22:13:49 +0800

    add packages

--- HiDinner/gen/com/sec/hidinner/R.java
+++ HiDinner/gen/com/sec/hidinner/R.java
@@ -29,6 +29,7 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int my_location=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
